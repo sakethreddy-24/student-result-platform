749a52d1f04ab58a4c060e091bae2a7106cb9c3a
sakethreddy-24 <sakethreddy1129@gmail.com>
2026-07-16 16:14:30 +0000

    Provision Azure Resource Group using Terraform

---
+++ terraform/azure/environments/production/main.tf
@@ -0,0 +1,6 @@
+module "resource_group" {
+  source = "../../modules/resource_group"
+
+  resource_group_name = var.resource_group_name
+  location            = var.location
+}
---
+++ terraform/azure/environments/production/outputs.tf
@@ -0,0 +1,7 @@
+output "resource_group_name" {
+  value = module.resource_group.name
+}
+
+output "location" {
+  value = module.resource_group.location
+}
